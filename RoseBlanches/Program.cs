1676180dd2f4b8c022dfdea62acb586056e11044
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-02-11 10:13:24 -0500

    Initial Code

--- RoseBlanches/Program.cs
+++ RoseBlanches/Program.cs
@@ -10,6 +10,39 @@ namespace RoseBlanches
     {
         static void Main(string[] args)
         {
+            double moneyReceived = 800;
+            double booksSupplies;
+            double otherPurchases;
+            int coffees;
+            int flashComputer;
+            int subwayTickets;
+            double bouquet = 0;
+
+            booksSupplies = moneyReceived * 3 / 4;
+
+            otherPurchases = (moneyReceived - booksSupplies) / 3;
+
+            coffees = (int)otherPurchases / 2;
+            bouquet += otherPurchases % 2;
+
+            subwayTickets = (int)otherPurchases / 3;
+            bouquet += otherPurchases % 3;
+
+            flashComputer = (int)otherPurchases / 4;
+            bouquet += otherPurchases % 4;
+            bouquet = (int)bouquet;
+
+            Console.WriteLine("How much did you receive money ($)? {0}", moneyReceived);
+            Console.WriteLine("");
+            Console.WriteLine("Books and Supplies: {0} $", booksSupplies);
+            Console.WriteLine("You can then buy: ");
+            Console.WriteLine("{0} coffees", coffees);
+            Console.WriteLine("");
+            Console.WriteLine("{0} Flash Computer Numbers", flashComputer);
+            Console.WriteLine("{0} subway Tickets", subwayTickets);
+            Console.WriteLine("and you will have {0} dollars for the white roses.", bouquet);
+
+
         }
     }
 }
